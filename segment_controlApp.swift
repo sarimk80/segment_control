b68d8ba64057821cb212c074cad13aeb4f6f781e
Sarim <sarimk80@gmail.com>
2021-10-16 10:35:16 +0500

    Initial commit

---
+++ segment_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  segment_controlApp.swift
+//  segment_control
+//
+//  Created by sarim khan on 16/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct segment_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
